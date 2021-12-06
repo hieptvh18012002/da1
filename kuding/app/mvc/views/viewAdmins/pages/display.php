c913270edfcb77b8df3fd766be069b28a01be4d6
hieptvh18012002 <hieptvhph14543@fpt.edu.vn>
2021-12-06 22:47:44 +0700

    ádasd

--- kuding/app/mvc/views/viewAdmins/pages/display.php
+++ kuding/app/mvc/views/viewAdmins/pages/display.php
@@ -5,34 +5,42 @@
             <p class="card-description">
                 Basic form elements
             </p>
-           
+           <?php if(isset($_GET['msg'])):?>
+            <div class="alert alert-success"><?= $_GET['msg'] ?></div>
+            <?php endif;?>
             <form id="form_display" class="forms-sample" method="POST" enctype="multipart/form-data">
             <div class="form-group">
                     <label for="">Tên website </label>
-                    <input type="text" placeholder="Nhập tên website" class="form-control" name="name">
+                    <input type="text" value="<?= $data['display']['web_name'] ?>" placeholder="Nhập tên website" class="form-control" name="web_name">
                 </div>
                 <div class="form-group">
                     <label for="">Logo website </label>
-                    <input type="file" class="form-control" name="logo">
+                    <img src="./public/images/layout/<?= $data['display']['logo'] ?>" alt="" width="200px">
+                    <p>Tải logo mới</p>
+                    <input type="file" class="form-control" id="upload" onchange="previewImg()" name="logo" >
+                    <input type="hidden" name="logo" value="<?= $data['display']['logo'] ?>">
+                    <?php if($data['er']):?>
+                        <div class="text-danger"><?= $data['er'] ?></div>
+                        <?php endif; ?>
+                    <div id="displayImg" class="" style="width: 200px;"> </div>
                 </div>
                 <div class="form-group">
                     <label class="mr-3" for="special1">Giới thiệu (homepage) </label>
                     <p>Tiêu đề</p>
-                    <input type="text" value="" name="title_intro" class="form-control">
+                    <input type="text" value="<?= $data['display']['title_intro'] ?>" name="title_intro" class="form-control">
                     <p>Nội dung</p>
-                    <textarea class="form-control" id="local-upload" name="desc" rows="5"></textarea>
+                    <textarea class="form-control" id="local-upload" name="content_intro" rows="5"><?= $data['display']['content_intro'] ?></textarea>
 
                 </div>
                 <div class="form-group">
                     <label for="">Quản lý url(social-footer)</label>
-                    <input type="text" placeholder="url facebook" name="fb_url" class="form-control">
-                    <input type="text" name="insta_url" placeholder="url instagram" class="form-control">
-                    <input type="text" name="twitter_url" class="form-control" placeholder="url twitter">
-                    <input type="text" name="pinterest_url" class="form-control" placeholder="url pinterest">
+                    <input type="text" value="<?= $data['display']['fb_url'] ?>" placeholder="url facebook" name="fb_url" class="form-control">
+                    <input type="text" value="<?= $data['display']['insta_url'] ?>" name="insta_url" placeholder="url instagram" class="form-control">
+                    <input type="text" value="<?= $data['display']['twitter_url'] ?>" name="twitter_url" class="form-control" placeholder="url twitter">
+                    <input type="text" value="<?= $data['display']['pinterest_url'] ?>" name="pinterest_url" class="form-control" placeholder="url pinterest">
                 </div>
-                
+
                 <button type="submit" class="btn btn-primary mr-2" name="btn_update">Submit</button>
-                <button class="btn btn-light">Cancel</button>
             </form>
         </div>
     </div>
